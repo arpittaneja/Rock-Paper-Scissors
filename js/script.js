3f0718e17f6f8b7c027714f90cf2efa7afc4f075
arpittaneja <88012@arsd.du.ac.in>
2021-12-30 12:41:46 +0530

    added comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+//function to return randomized computer choice
 function computerPlay() {
-    const randInt = Math.floor(Math.random() * 3) + 1;
+    const randInt = Math.floor(Math.random() * 3) + 1; //random number between 1, 2 and 3
     let computerPlay = undefined;
 
     if (randInt == 1) computerPlay = "Rock";
@@ -8,8 +9,9 @@ function computerPlay() {
     return computerPlay;
 }
 
+//function to decide the outcome of each round
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase(); //conversion of all types of inputs to lowercase
     computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection == "rock" && computerSelection == "rock") return "Round Draw";
@@ -31,27 +33,33 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection == "scissors" && computerSelection == "paper") return "You Win! Scissors defeat Paper";
 }
 
+//main function which loops each round five times
 function game() {
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = window.prompt(`Round ${i + 1}
 
-Rock, Paper, Scissors?`);
+    for (let i = 0; i < 5; i++) {
+        //prompt for user input
+        const playerSelection = window.prompt(`Round ${i + 1}  
+
+Rock, Paper, Scissors?`); 
+        
         const computerSelection = computerPlay();
         const result = playRound(playerSelection, computerSelection);
+
+        //displays scores after each round
         if (result.slice(0, 8) == "You Win!") playerScore++;
         else if (result.slice(0, 9) == "You Lose!") computerScore++;
         window.alert(computerSelection);
         window.alert(`${result}
 
-Your Score: ${playerScore}
+Your Score: ${playerScore} 
 Computer Score: ${computerScore}`);
     }
 
+    //displays result if match
     if (playerScore > computerScore) window.alert("You Won this match!")
     else if (computerScore > playerScore) window.alert("You lost this match!")
-
     else window.alert("Match Drawn")
 }
 
