9723a768b7414eb23963b4152d388470ec2eef27
Arpit Taneja <88012@arsd.du.ac.in>
2022-01-03 13:58:20 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 //function to return randomized computer choice
 function computerPlay() {
-    const randInt = Math.floor(Math.random() * 3) + 1; //random number between 1, 2 and 3
-    let computerPlay = undefined;
+    const randInt = Math.floor(Math.random() * 3); //random number between 1, 2 and 3
 
-    if (randInt == 1) computerPlay = "rock";
-    else if (randInt == 2) computerPlay = "paper";
-    else if (randInt == 3) computerPlay = "scissors";
+    let choiceArray = ["rock", "paper", "scissors"]; //array of all possible choices
+
+    let computerPlay = choiceArray[randInt]; //conversion of random number to corresponding choice
     return computerPlay;
 }
 
-//function to decide and return the outcome of each round
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == null || playerSelection == undefined) return "Exit"; //checking if player selection is null or undefined
 
-    playerSelection = playerSelection.toLowerCase(); //conversion of all types of inputs to lowercase
+//function to calculate and return the outcome of each case
+function playRound(playerSelection, computerSelection) {
+    // if (playerSelection == null || playerSelection == undefined) return "Exit"; //checking if player selection is null or undefined
+
+    // playerSelection = playerSelection.toLowerCase(); //conversion of all types of inputs to lowercase
 
     if (playerSelection == computerSelection) return "Round Draw!";
     else if (playerSelection == "rock" && computerSelection == "paper") return "You Lose! Paper defeats Rock!";
@@ -25,59 +25,52 @@ function playRound(playerSelection, computerSelection) {
     else return "Please enter a valid move!";
 }
 
-//main function which loops each round five times
-function game() {
-    //initializing scores to zero
-    let playerScore = 0; 
-    let computerScore = 0;
+//takes in value of playerSelection and updates the document accordingly
+function updatePage(playerSelection) {
+    const computerSelection = computerPlay();
+    const result = playRound(playerSelection, computerSelection);
 
-    //initializing round number to 1
-    let roundNumber = 1;
+    if (result.startsWith("You Win!")) {
+        playerScore++;
+        roundNumber++;
 
-    //loop until round number is not equal to six
-    while (roundNumber != 6) {
-        //prompt for user input
-        const playerSelection = prompt(`Round ${roundNumber}
+        const playerCounter = document.querySelector("div .player-score .player-score-box");
+        playerCounter.innerHTML = playerScore;
+        console.log(result);
+        console.log("Player Score: " + playerScore);
+        console.log("Computer Score: " + computerScore);
+        }
 
-Rock, Paper, Scissors?`);
+    else if (result.startsWith("You Lose!")) {
+        computerScore++;
+        roundNumber++;
 
-        const computerSelection = computerPlay();
-        const result = playRound(playerSelection, computerSelection);
-
-        if (result.startsWith("You Win!")) playerScore++, roundNumber++;
-        else if (result.startsWith("You Lose!")) computerScore++, roundNumber++;
-        else if (result.startsWith("Round Draw!")) roundNumber++;
-        else if (result == "Exit") break;
-        else roundNumber = roundNumber;
-
-        //displays scores after each round
-        alert(`${result}
-
-Your Score: ${playerScore} 
-Computer Score: ${computerScore}`);
+        const computerCounter = document.querySelector("div .computer-score .computer-score-box");
+        computerCounter.innerHTML = computerScore;
+        console.log(result);
+        console.log("Player Score: " + playerScore);
+        console.log("Computer Score: " + computerScore);
     }
 
-    //displays result of match
-    if (playerScore > computerScore) alert(`You won this match!
-
-Score after all rounds:
-Your Score: ${playerScore} 
-Computer Score: ${computerScore}`)
-        
-        
-    else if (computerScore > playerScore) alert(`You lost this match!
-
-Score after all rounds:
-Your Score: ${playerScore} 
-Computer Score: ${computerScore}`)
-        
-        
-    else alert(`Match Drawn!
-
-Score after all rounds:
-Your Score: ${playerScore} 
-Computer Score: ${computerScore}`)
+    else if (result.startsWith("Round Draw!")) {
+        console.log(result);
+        roundNumber++;
+    }
 }
 
-//calling the main game function
-game();
+//driver code
+//initializing both scores to zero
+let playerScore = 0;
+let computerScore = 0;
+
+//initializing round number to 1
+let roundNumber = 1;
+
+//selects an array of all buttons
+const buttons = Array.from(document.querySelectorAll(".selection"));
+
+//applies eventListener to all the buttons
+buttons.forEach(button => button.addEventListener("click", function (button) {
+    const playerSelection = button.target.id;
+    updatePage(playerSelection);
+}));
