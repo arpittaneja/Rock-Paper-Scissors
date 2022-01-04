f0db4dbb35d767cc0351ea623940338d83356b2f
Arpit Taneja <88012@arsd.du.ac.in>
2022-01-04 21:40:24 +0530

    frontend and backend fully working

--- js/script.js
+++ js/script.js
@@ -24,45 +24,49 @@ function playRound(playerSelection, computerSelection) {
     else return "Please enter a valid move!";
 }
 
-//takes in value of playerSelection and updates the document accordingly
+//takes in value of playerSelection and updates the whole page accordingly
 function updatePage(playerSelection) {
+
+    //increments round number per click
+    roundNumber++;
+
+    startText.textContent = `Round: ${roundNumber}`;
+
     const computerSelection = computerPlay();
     const result = playRound(playerSelection, computerSelection);
 
-    console.log("Player Selection: " + playerSelection);
-    console.log("Computer Selection: " + computerSelection);
+    // console.log("Player Selection: " + playerSelection);
+    // console.log("Computer Selection: " + computerSelection);
+
+    //updates result to the resut element
     resultPanel.textContent = result;
 
     if (result.startsWith("You Win!")) {
         playerScore++;
-        roundNumber++;
-
-
         playerCounter.textContent = playerScore;
-        console.log(result);
-        console.log("Player Score: " + playerScore);
-        console.log("Computer Score: " + computerScore);
+        // console.log(result);
+        // console.log("Player Score: " + playerScore);
+        // console.log("Computer Score: " + computerScore);
     }
 
     else if (result.startsWith("You Lose!")) {
         computerScore++;
-        roundNumber++;
-
         computerCounter.textContent = computerScore;
-        console.log(result);
-        console.log("Player Score: " + playerScore);
-        console.log("Computer Score: " + computerScore);
+        // console.log(result);
+        // console.log("Player Score: " + playerScore);
+        // console.log("Computer Score: " + computerScore);
     }
 
     else if (result.startsWith("Round Draw!")) {
         console.log(result);
-        roundNumber++;
     }
 
     if (playerScore == 5 || computerScore == 5) {
-        console.log("stop");
-        playAgain.style.display = 'block';
+        // console.log("stop");
         disableSelectionButtons();
+        startText.style.display = "none";
+        playAgain.style.display = "block";
+
         if (playerScore > computerScore) {
             resultPanel.textContent = "You Win This Match";
         }
@@ -72,21 +76,28 @@ function updatePage(playerSelection) {
     }
 }
 
+//disables all the selection buttons when any score reaches 5 
 function disableSelectionButtons() {
     for (button of buttons) {
         button.disabled = true;
     }
 }
 
+//updates the page accordingly when play again button is pressed
 function anotherGame() {
+    //reinitializes both scores to zero
     playerScore = computerScore = 0;
+    roundNumber = 1;
+    playAgain.style.display = "none";
+    startText.style.display = "block";
     playerCounter.textContent = playerScore;
     computerCounter.textContent = computerScore;
-    playAgain.style.display = 'none';
+    startText.textContent = `Round: ${roundNumber}`;
     resultPanel.textContent = "";
     enableSelectionButtons();
 }
 
+//enables all selection buttons when new game is started
 function enableSelectionButtons() {
     for (button of buttons) {
         button.disabled = false;
@@ -99,26 +110,30 @@ function enableSelectionButtons() {
 let playerScore = 0;
 let computerScore = 0;
 
-//initializing round number to 1
-let roundNumber = 1;
+//initializing round number to 0
+let roundNumber = 0;
 
 //selects an array of all buttons
 const buttons = Array.from(document.querySelectorAll(".selection"));
 
-
+//selects the element which displays the result
 const resultPanel = document.querySelector(".result p");
-const playerCounter = document.querySelector("div .player-score .player-score-number");
 
+//selects the element that displays both scores
+const playerCounter = document.querySelector("div .player-score .player-score-number");
+const computerCounter = document.querySelector("div .computer-score .computer-score-number");
+
+//selects the element which displays start text and round number
 const startText = document.querySelector(".start-text");
-//applies eventListener to all the buttons
+
+//applies eventListener to all the selection buttons
 buttons.forEach(button => button.addEventListener("click", function (button) {
-    startText.style.display = "none";
     const playerSelection = button.target.id;
     updatePage(playerSelection);
 }));
 
+//applies eventListener to start again button
 const playAgain = document.querySelector('.again .play-again');
 playAgain.addEventListener('click', anotherGame);
 
-const computerCounter = document.querySelector("div .computer-score .computer-score-number");
 
